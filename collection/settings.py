82dde35164b76e390616574ba2890755a549fc3a
Godfrey Ssemombwe <Ssemombwegodfrey77@gmail.com>
2025-09-22 19:39:04 +0300

    Update settings for Render deployment

--- collection/settings.py
+++ collection/settings.py
@@ -3,6 +3,7 @@ Django settings for trussybridal project.
 """
 
 from pathlib import Path
+import os
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -10,7 +11,8 @@ SECRET_KEY = 'django-insecure-mlt*ju+hiphlm-=61(z5kc3e!z^41_@fe!^qct6_a#+d1bed$3
 
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# Allow all hosts for now (you can replace * with your Render domain later)
+ALLOWED_HOSTS = ["*"]
 
 INSTALLED_APPS = [
     'django.contrib.admin',
@@ -32,16 +34,17 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-# Fix ROOT_URLCONF to your project-level urls.py (likely 'trussybridal.urls' instead of 'collection.urls')
+# Make sure this points to your project urls.py (check your folder structure)
 ROOT_URLCONF = 'collection.urls'
 
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],  # you can add project-level templates dir here if you have one
+        'DIRS': [BASE_DIR / "templates"],  # safe to include, even if empty
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
+                'django.template.context_processors.debug',
                 'django.template.context_processors.request',
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
@@ -71,12 +74,13 @@ LANGUAGE_CODE = 'en-us'
 TIME_ZONE = 'UTC'
 
 USE_I18N = True
-
 USE_TZ = True
 
+# Static files (CSS, JS, images)
 STATIC_URL = '/static/'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 
-# Media settings for uploaded files
+# Media files
 MEDIA_URL = '/media/'
 MEDIA_ROOT = BASE_DIR / 'media'
 
